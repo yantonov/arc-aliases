68842544ec32b5ca37cb8f968384fe52f0f3cddd
Yury Antonov <yantonov@yandex.ru>
2020-02-09 19:13:46 +0500

    show umount notification

--- arc-umount.sh
+++ arc-umount.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+check="$(arc br)"
+
+if [ $? -eq 0 ]; then
+    echo "You are inside the arc repository, leave it before run unmount command"
+    exit 1
+fi
+
 cd $ARC_ROOT
 
 nice arc unmount arcadia/
